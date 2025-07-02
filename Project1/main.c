289406e2404ceda5107a988a413bea23eb0b0073
Paul <over1ordrtxx@gmail.com>
2025-07-02 20:18:37 +0300

    Доработал код
    Помимо того обеспечил корректность и правильность чтения согласно нормам орфографии и грамматики русского языка

--- Project1/main.c
+++ Project1/main.c
@@ -7,6 +7,7 @@
 #include <time.h>
 
 int size, choice;
+int* arr = 0;
 char n_file[49];
 FILE* file;
 
@@ -24,15 +25,15 @@ void bubbleSort(int arr[], int size) {
 }
 
 void writeArrayToFile(int arr[], int size) {
-    printf("Введите имя файла для загрузки данных в него: ");
+    printf("Введите имя файла для сохранения массива в файл: ");
     scanf("%s", n_file);
     if (sizeof(n_file) > 49) {
-        printf("Имя файла превышает доступное значение (49)!\n");
+        printf("Имя файла превышает максимально допустимую длину (49)!\n");
         return;
     }
     if ((file = fopen(n_file, "w")) == NULL)
     {
-        printf("\nНевозможно открыть для записи файл: %s\n", n_file);
+        printf("\nНевозможно открыть файл для записи: %s\n", n_file);
         _getch();
         return;
     }
@@ -41,7 +42,28 @@ void writeArrayToFile(int arr[], int size) {
         fprintf(file, "%d ", arr[i]);
 
     fclose(file);
-    printf("Данные успешно записаны!\n");;
+    printf("Запись массива завершена!\n");
+}
+void readArrayFromFile() {
+    printf("Введите имя файла для чтения массива из файла: ");
+    scanf("%s", n_file);
+    if (sizeof(n_file) > 49) {
+        printf("Имя файла превышает максимально допустимую длину (49)!");
+        return;
+    }
+    if ((file = fopen(n_file, "r+")) == 0)
+    {
+        printf("Невозможно открыть файл для чтения %s\n", n_file);
+        _getch();
+        return;
+    }
+    fscanf(file, "%d", &size);
+    arr = (int*)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++) {
+        fscanf(file, "%d", &arr[i]);
+    }
+    fclose(file);
+    printf("Чтение массива завершено!\n");
 }
 
 void helloScreen() {
@@ -56,38 +78,27 @@ void main() {
     clock_t start, end;
     double time_spent = 0.0;
     system("cls");
-    printf("Для начала нужно создать массив\n");
-    printf("Введите размер массива: ");
-    scanf("%d", &size);
-
-    int* arr = (int*)malloc(size * sizeof(int));
-    if (arr == NULL) {
-        printf("Не удалось выделить память!\n");
-        return;
-    }
-    for (int i = 0; i < size; i++) {
-        arr[i] = rand();
-    }
-    printf("Массив создан\n");
-    
     do {
-        printf("1. Просмотреть массив\n");
+        printf("\tМеню\n");
+        printf("1. Показать массив\n");
         printf("2. Отсортировать массив\n");
         printf("3. Записать массив в файл\n");
-        printf("4. Посмотреть время выполнения алгоритма\n");
-        printf("5. Выход\n");
-        printf("Выберите действие: ");
-
+        printf("4. Считать массив из файла\n");
+        printf("5. Показать время сортировки\n");
+        printf("6. Создать новый массив\n");
+        printf("7. Выход\n");
+        printf("Введите выбор: ");
         scanf("%d", &choice);
         switch (choice) {
-        case 1: //Просмотр
-            printf("Массив: ");
+        case 1:
+            system("cls");
+            printf("Массив:\n ");
             for (int i = 0; i < size; i++) {
-                printf("%d ", arr[i]);
+                printf("%d) %d\n ", i+1, arr[i]);
             }
             printf("\n");
             break;
-        case 2: // Сортировка
+        case 2:
             system("cls");
             start = clock();
             bubbleSort(arr, size);
@@ -96,29 +107,50 @@ void main() {
             time_spent += (double)(end - start) / CLOCKS_PER_SEC;
             printf("\n");
             break;
-        case 3: // Запись в файл
+        case 3:
             system("cls");
             writeArrayToFile(arr, size);
             break;
-        case 4: // Время
+        case 4:
+            system("cls");
+            if (arr != NULL) 
+            {
+                free(arr);
+            }
+            readArrayFromFile();
+            break;
+        case 5:
             system("cls");
             if (time_spent == 0) {
                 printf("Сначала отсортируйте массив!\n");
                 break;
             }
-             printf("Время выполнения алгоритма: %f\n", time_spent);
+             printf("Общее время сортировки: %f\n", time_spent);
              break;
-        case 5: // Выход
+        case 6:
+            system("cls");
+            free(arr);
+            printf("Введите размер массива: ");
+            scanf("%d", &size);
+            arr = (int*)malloc(size * sizeof(int));
+            if (arr == NULL) {
+                printf("Не удалось выделить память!\n");
+                return;
+            }
+            for (int i = 0; i < size; i++) {
+                arr[i] = rand();
+            }
+            break;
+        case 7:
              system("cls");
              printf("До свидания!\n");
-             //free(arr);
              break;
         default:
              system("cls");
-             printf("Некорректный выбор!\n");
+             printf("Некорректный ввод!\n");
              break;
         }
-    } while (choice != 5);
+    } while (choice != 7);
 
     getchar();
 }
